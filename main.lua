81c43b378fb5c9d19ca270866e6db54f9be58dfb
Yuto Suzuki <yuutoo.advance@gmail.com>
2014-07-12 12:54:22 +0900

    remove some comment

--- main.lua
+++ main.lua
@@ -11,17 +11,3 @@ local composer = require "composer"
 -- load first scene
 composer.gotoScene("compass", "fade", 400)
 
---[[ Uncomment to monitor app's lua memory/texture memory usage in terminal...
-
-local function garbagePrinting()
-	collectgarbage("collect")
-    local memUsage_str = string.format( "memUsage = %.3f KB", collectgarbage( "count" ) )
-    print( memUsage_str )
-    local texMemUsage_str = system.getInfo( "textureMemoryUsed" )
-    texMemUsage_str = texMemUsage_str/1000
-    texMemUsage_str = string.format( "texMemUsage = %.3f MB", texMemUsage_str )
-    print( texMemUsage_str )
-end
-
-Runtime:addEventListener( "enterFrame", garbagePrinting )
---]]
