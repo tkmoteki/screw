799b54056499f50ba9b4447d47a1dc2302fb5bb1
Yuto Suzuki <yuutoo.advance@gmail.com>
2014-07-12 12:51:17 +0900

    make infinity roulette

--- main.lua
+++ main.lua
@@ -4,4 +4,24 @@
 -- Screw is MIT licensed
 -- Copyright (C) 2014 CyberZ Inc. All Right Reserved.
 
-display.setDefault("background", 80/255)
+display.setDefault(display.HiddenStatusBar)
+
+local composer = require "composer"
+
+-- load first scene
+composer.gotoScene("compass", "fade", 400)
+
+--[[ Uncomment to monitor app's lua memory/texture memory usage in terminal...
+
+local function garbagePrinting()
+	collectgarbage("collect")
+    local memUsage_str = string.format( "memUsage = %.3f KB", collectgarbage( "count" ) )
+    print( memUsage_str )
+    local texMemUsage_str = system.getInfo( "textureMemoryUsed" )
+    texMemUsage_str = texMemUsage_str/1000
+    texMemUsage_str = string.format( "texMemUsage = %.3f MB", texMemUsage_str )
+    print( texMemUsage_str )
+end
+
+Runtime:addEventListener( "enterFrame", garbagePrinting )
+--]]
